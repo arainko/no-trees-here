bb38d6c9187d08449729e34bbe02178088c243f6
Aleksander Rainko <aleksander.rainko99@gmail.com>
2024-06-08 09:10:45 +0200

    sequencer

---
+++ Sequence.scala
@@ -0,0 +1,21 @@
+type Sequence[T <: Tuple, F[_]] =
+  F[Tuple.InverseMap[T, F]]
+
+object Sequence {
+  def run[T <: Tuple: Tuple.IsMappedBy[F], F[_]](
+    value: T,
+    f: [a] => List[F[a]] => F[List[a]],
+    map: [a, b] => (fa: F[a], f: a => b) => F[b]
+  ): Sequence[T, F] = {
+    val values = value.productIterator.asInstanceOf[Iterator[F[Any]]].toList
+    map(f(values), list => Tuple.fromArray(list.toArray)).asInstanceOf[Sequence[T, F]]
+  }
+
+  // type a = Tuple.IsMappedBy[]
+
+  type Input = (Option[Int], Option[String], Option[Double], Option[Float])
+
+  type Output = Option[(Int, String, Double, Float)]
+
+  val cos = run[Input, Option]((Some(1), Some("Str"), Some(1d), Some(1f)), [a] => (list: List[Option[a]]) => list.reduce(_ zip _))
+}
